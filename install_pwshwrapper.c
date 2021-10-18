6fff91794d39d385ae05ea778d929777326dbe42
PietJankbal <xerox.xerox2000x@gmail.com>
2021-10-19 00:49:05 +0200

    Update install_pwshwrapper.c

--- install_pwshwrapper.c
+++ install_pwshwrapper.c
@@ -17,36 +17,18 @@
  * Compile: *
  * x86_64-w64-mingw32-gcc -municode  -mconsole install_pwshwrapper.c -lurlmon -luser32 -lntdll -s -o install_pwshwrapper.exe
  */
-#define WIN32_LEAN_AND_MEAN
-#define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
-
 #include <windows.h>
-#include <winuser.h>
-#include <winternl.h>
-#include <winbase.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <urlmon.h>
-#include <wchar.h>
 
 int __cdecl wmain(int argc, WCHAR *argv[])
 {
     WCHAR dest64W[MAX_PATH];WCHAR dest32W[MAX_PATH];
 
-    if(!ExpandEnvironmentStringsW(L"%systemroot%", dest64W, MAX_PATH+1)) goto failed;
-    if(!ExpandEnvironmentStringsW(L"%systemroot%", dest32W, MAX_PATH+1)) goto failed;
-      
-    lstrcatW(dest64W, L"\\system32\\WindowsPowerShell\\v1.0\\powershell.exe");
-    lstrcatW(dest32W, L"\\syswow64\\WindowsPowerShell\\v1.0\\powershell.exe");
-
+    if(!ExpandEnvironmentStringsW(L"%systemroot%", dest64W, MAX_PATH+1)) goto failed; if(!ExpandEnvironmentStringsW(L"%systemroot%", dest32W, MAX_PATH+1)) goto failed;
+    lstrcatW(dest64W, L"\\system32\\WindowsPowerShell\\v1.0\\powershell.exe"); lstrcatW(dest32W, L"\\syswow64\\WindowsPowerShell\\v1.0\\powershell.exe");
     DeleteFileW(dest64W); DeleteFileW(dest32W);
-
-    if( URLDownloadToFileW(NULL, L"https://raw.githubusercontent.com/PietJankbal/powershell-wrapper-for-wine/master/powershell64.exe", dest64W, 0, NULL) != S_OK )
-        goto failed;
-   
-    if( URLDownloadToFileW(NULL, L"https://raw.githubusercontent.com/PietJankbal/powershell-wrapper-for-wine/master/powershell32.exe", dest32W, 0, NULL) != S_OK )
-        goto failed;
-
+    if( URLDownloadToFileW(NULL, L"https://raw.githubusercontent.com/PietJankbal/powershell-wrapper-for-wine/master/powershell64.exe", dest64W, 0, NULL) != S_OK ) goto failed;
+    if( URLDownloadToFileW(NULL, L"https://raw.githubusercontent.com/PietJankbal/powershell-wrapper-for-wine/master/powershell32.exe", dest32W, 0, NULL) != S_OK ) goto failed;
     fwprintf(stderr, L"\033[1;93m"); fwprintf(stderr, L"\nFiles successfully installed; Do 'wine powershell' to complete the installation \n"); fwprintf(stderr, L"\033[0m\n");
 
     return 0;
