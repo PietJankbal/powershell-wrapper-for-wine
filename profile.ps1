832f3c8dfdce445ce62f23d92fbdf444e15f2138
PietJankbal <xerox.xerox2000x@gmail.com>
2021-11-20 22:08:27 +0100

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -44,6 +44,22 @@ Function Get-WmiObject([parameter(mandatory)] [string]$class, [string[]]$propert
 
 Set-Alias Get-CIMInstance Get-WMIObject
 
+#Example (works on windows,requires admin rights): Set-WmiInstance -class win32_operatingsystem -arguments @{"description" = "MyDescription"}
+#Based on https://devblogs.microsoft.com/scripting/use-the-set-wmiinstance-powershell-cmdlet-to-ease-configuration/
+Function Set-WmiInstance( [string]$class, [hashtable]$arguments, [string]$computername = "localhost", `
+                          [string]$namespace = "root\cimv2" <#, [string]$path #>)
+{
+    $assembledpath = "\\" + $computername + "\" + $namespace + ":" + $class
+    $obj = ([wmiclass]"$assembledpath").CreateInstance()
+
+    foreach ($h in $arguments.GetEnumerator()) {
+        $obj.$($h.Name) = $($h.Value)
+    }
+    $result = $obj.put()
+
+    return $result.Path
+}
+
 #Note: Following obviously overrides wine (-staging)`s tasklist(.exe) so just remove stuff below if you don`t want that 
 New-Alias tasklist.exe tasklist
 
