d16a8de0a2ea08538bb797e5e1e7999eb8b7e5cb
PietJankbal <xerox.xerox2000x@gmail.com>
2024-03-11 17:04:59 -0400

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -22,37 +22,30 @@ function Register-WMIEvent
 
 #Prerequisite: Stuff below requires native dotnet (winetricks -q dotnet48) to be installed, otherwise it will just fail!
 #
-#Based on Get-WmiCustom by Daniele Muscetta, so credits to aforementioned author;
-#See https://www.powershellgallery.com/packages/Traverse/0.6/Content/Private%5CGet-WMICustom.ps1
-#
 #Only works as of wine-6.20, see https://bugs.winehq.org/show_bug.cgi?id=51871
 #
 #Examples of usage:
-#
-#Get-WmiObject win32_operatingsystem version
 #$(Get-WmiObject win32_videocontroller).name
-Function Get-WmiObject([parameter(mandatory)] [string]$class, [string[]]$property="*", `
+Function Get-WmiObject([parameter(mandatory=$true, position = 0, parametersetname = 'class')] [string]$class, `
+                       [parameter( position = 1, parametersetname = 'class')][string[]]$property="*", `
                        [string]$computername = "localhost", [string]$namespace = "root\cimv2", `
-                       [string]$filter<#not used yet, but otherwise chocolatey starts complaining#>)
+                       [string]$filter, [parameter(parametersetname = 'query')] [string]$query)
 {
     if (!(Test-Path  "HKLM:\Software\Microsoft\.NETFramework\v4.0.30319"))
     {  Add-Type -AssemblyName PresentationCore,PresentationFramework; [System.Windows.MessageBox]::Show('This function requires native .NET48 to be installed!   Do "winetricks -q dotnet48" and try again ','Warning','ok','exclamation'); return}
+    if(!$query) {    
+        $query = "SELECT " +  $($property | Join-String -Separator ",") + " FROM " + $class + (($filter) ? (' where ' + $filter ) : ('')) }
 
-    $ConnectionOptions = new-object System.Management.ConnectionOptions
-    $assembledpath = "\\" + $computername + "\" + $namespace
-    
-    $Scope = new-object System.Management.ManagementScope $assembledpath, $ConnectionOptions
-    $Scope.Connect() 
-    
-    $querystring = "SELECT " +  $property + " FROM " + $class
-    $query = new-object System.Management.ObjectQuery $querystring
-    $searcher = new-object System.Management.ManagementObjectSearcher
-    $searcher.Query = $querystring
-    $searcher.Scope = $Scope 
-    
-    return $searcher.get()
+    $searcher = [wmisearcher]$query
+
+    $searcher.scope.path = "\\" + $computername + "\" + $namespace
+
+    return [System.Management.ManagementObjectCollection]$searcher.get()
 }
 
-Set-Alias Get-CIMInstance Get-WMIObject
+Function Get-CIMInstance ( [parameter(mandatory)] [string]$classname, [string[]]$property="*", [string]$filter)
+{
+     Get-WMIObject $classname -property $property -filter $filter
+}
 
-
+Set-Alias gwmi Get-WmiObject
